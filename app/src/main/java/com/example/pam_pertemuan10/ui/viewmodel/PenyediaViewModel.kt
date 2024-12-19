9f7823621aa9020843e064ab9a5d8573c12f2303
DaffaPutra7 <putradika3604@gmail.com>
2024-12-19 20:49:19 +0700

    Membuat file PenyediaViewModel di package viewmodel yang didalamnya terdapat fungsi CreationExtras.krsApp() dan object PenyediaViewModel

---
+++ app/src/main/java/com/example/pam_pertemuan10/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.pam_pertemuan10.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pam_pertemuan10.ui.theme.KrsApp
+
+object PenyediaViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            InsertDosenViewModel(
+                krsApp().containerApp.repositoryDosen
+            )
+        }
+
+        initializer {
+            HomeDosenViewModel(
+                krsApp().containerApp.repositoryDosen
+            )
+        }
+    }
+}
+
+fun CreationExtras.krsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
