9a66c1e1332cb548697260dcd64c23555493326b
DaffaPutra7 <putradika3604@gmail.com>
2024-12-20 22:33:21 +0700

    Menambah code pada file PengelolaHalaman.kt berupa composable untuk DestinasiHomeMK, DestinasiInsertMK, DestinasiDetailMK, dan DestinasiUpdateMK

--- app/src/main/java/com/example/pam_pertemuan10/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_pertemuan10/ui/navigation/PengelolaHalaman.kt
@@ -3,12 +3,19 @@ package com.example.pam_pertemuan10.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.pam_pertemuan10.ui.view.dosen.DestinasiInsertDosen
 import com.example.pam_pertemuan10.ui.view.dosen.HomeDosenView
 import com.example.pam_pertemuan10.ui.view.dosen.InsertDosenView
+import com.example.pam_pertemuan10.ui.view.matakuliah.DestinasiInsertMK
+import com.example.pam_pertemuan10.ui.view.matakuliah.DetailMKView
+import com.example.pam_pertemuan10.ui.view.matakuliah.HomeMKView
+import com.example.pam_pertemuan10.ui.view.matakuliah.InsertMkView
+import com.example.pam_pertemuan10.ui.view.matakuliah.UpdateMKView
 
 @Composable
 fun PengelolaHalaman(
@@ -19,6 +26,7 @@ fun PengelolaHalaman(
         navController = navController,
         startDestination = AlamatNavigasi.DestinasiHomeDosen.route
     ) {
+
         composable(
             route = AlamatNavigasi.DestinasiHomeDosen.route
         ){
@@ -41,6 +49,83 @@ fun PengelolaHalaman(
                     navController.popBackStack()
                 },
                 modifier = modifier
+            )
+        }
+
+        composable(
+            route = AlamatNavigasi.DestinasiHomeMK.route
+        ){
+            HomeMKView(
+                onDetailClick = { kode ->
+                    navController.navigate("${AlamatNavigasi.DestinasiDetailMK.route}/$kode")
+                    println(
+                        "PengelolaHalaman: kode = $kode"
+                    )
+                },
+                onAddMk = {
+                    navController.navigate(DestinasiInsertMK.route)
+                },
+                modifier = modifier
+            )
+        }
+
+        composable(
+            route = DestinasiInsertMK.route
+        ){
+            InsertMkView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
+            )
+        }
+
+        composable(
+            AlamatNavigasi.DestinasiDetailMK.routeWithArg,
+            arguments = listOf(
+                navArgument(AlamatNavigasi.DestinasiDetailMK.Kode) {
+                    type = NavType.StringType
+                }
+            )
+        ){
+            val kode = it.arguments?.getString(AlamatNavigasi.DestinasiDetailMK.Kode)
+
+            kode?.let { kode ->
+                DetailMKView(
+                    onBack = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${AlamatNavigasi.DestinasiUpdateMK.route}/$it")
+                    },
+                    modifier = modifier,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+
+        composable(
+            AlamatNavigasi.DestinasiUpdateMK.routeWithArg,
+            arguments = listOf(
+                navArgument(AlamatNavigasi.DestinasiUpdateMK.Kode) {
+                    type = NavType.StringType
+                }
+            )
+        ){
+
+            UpdateMKView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
             )
         }
     }
