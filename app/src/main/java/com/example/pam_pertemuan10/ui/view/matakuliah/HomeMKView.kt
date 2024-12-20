8e77a12ff74c647914da3cda4a4dd00f062b8288
DaffaPutra7 <putradika3604@gmail.com>
2024-12-20 19:45:31 +0700

    Membuat package matakuliah didalam package view, setelah itu membuat file HomeMKView.kt didalam package matakuliah. Pada file HomeMKView.kt terdapat fungsi CardMk, fungsi ListMataKuliah, fungsi BodyHomeMKView dan fungsi HomeMKView

---
+++ app/src/main/java/com/example/pam_pertemuan10/ui/view/matakuliah/HomeMKView.kt
@@ -0,0 +1,210 @@
+package com.example.pam_pertemuan10.ui.view.matakuliah
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_pertemuan10.data.entity.MataKuliah
+import com.example.pam_pertemuan10.ui.customwidget.TopAppBar
+import com.example.pam_pertemuan10.ui.viewmodel.HomeMKUiState
+import com.example.pam_pertemuan10.ui.viewmodel.HomeMKViewModel
+import com.example.pam_pertemuan10.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeMKView(
+    viewModel: HomeMKViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMk: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                onBack = { },
+                showBackButton = false,
+                judul = "Daftar Mata Kuliah",
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMk,
+                shape = MaterialTheme.shapes.medium,
+                modifier = modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mata Kuliah",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeMKUiState by viewModel.homeMKUiState.collectAsState()
+
+        BodyHomeMKView(
+            homeMKUiState = homeMKUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun BodyHomeMKView(
+    homeMKUiState: HomeMKUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        homeMKUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+
+        homeMKUiState.isError -> {
+            LaunchedEffect(homeMKUiState.errorMessage) {
+                homeMKUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+
+        homeMKUiState.listMk.isEmpty() -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data Mata Kuliah.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            ListMataKuliah(
+                listMk = homeMKUiState.listMk,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun ListMataKuliah(
+    listMk: List<MataKuliah>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+){
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = listMk,
+            itemContent = { mk ->
+                CardMk(
+                    mk = mk,
+                    onClick = { onClick(mk.kode) }
+                )
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardMk(
+    mk: MataKuliah,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = "Mata Kuliah: ${mk.nama}",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = "Kode: ${mk.kode}",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = "SKS: ${mk.sks}",
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
