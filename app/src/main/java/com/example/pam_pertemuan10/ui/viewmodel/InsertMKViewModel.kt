fe1be13793da10ea9db80283a45fab88f6238cc5
DaffaPutra7 <putradika3604@gmail.com>
2024-12-20 17:19:28 +0700

    Membuat file baru InsertMKViewModel.kt didalam package viewmodel yang berisikan data class MkEvent, fungsi Mk.Event.toMkEntity, data class MkErrorState, data class MkUiState dan class InsertMKViewModel

---
+++ app/src/main/java/com/example/pam_pertemuan10/ui/viewmodel/InsertMKViewModel.kt
@@ -0,0 +1,95 @@
+package com.example.pam_pertemuan10.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_pertemuan10.data.entity.MataKuliah
+import com.example.pam_pertemuan10.repository.RepositoryMK
+import kotlinx.coroutines.launch
+
+class InsertMKViewModel(private val repositoryMK: RepositoryMK) : ViewModel() {
+    var mkState by mutableStateOf(MkUiState())
+
+    fun updateMkState(mkEvent: MkEvent){
+        mkState = mkState.copy(
+            mkEvent = mkEvent
+        )
+    }
+
+    private fun validateMkFields(): Boolean{
+        val event = mkState.mkEvent
+        val errorState = MkErrorState(
+            kode = if (event.kode.isNotEmpty()) null else "Kode tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            sks = if (event.sks.isNotEmpty()) null else "SKS tidak boleh kosong",
+            semester = if (event.semester.isNotEmpty()) null else "Semester tidak boleh kosong",
+            jenis = if (event.jenis.isNotEmpty()) null else "Jenis tidak boleh kosong",
+            dosenPengampu = if (event.dosenPengampu.isNotEmpty()) null else "Dosen Pengampu tidak boleh kosong"
+        )
+        mkState = mkState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+    fun saveDataMk(){
+        val currentEvent = mkState.mkEvent
+        if (validateMkFields()){
+            viewModelScope.launch {
+                try {
+                    repositoryMK.insertMataKuliah(currentEvent.toMkEntity())
+                    mkState = mkState.copy(
+                        snackBarMessage = "Data Berhasil Disimpan",
+                        mkEvent = MkEvent(),
+                        isEntryValid = MkErrorState()
+                    )
+                } catch (e: Exception) {
+                    mkState = mkState.copy(snackBarMessage = "Data Gagal Disimpan")
+                }
+            }
+        } else{
+            mkState = mkState.copy(snackBarMessage = "Input tidak valid. Periksan kembali data Anda.")
+        }
+    }
+    fun  resetSnackBarMessage(){
+        mkState = mkState.copy(snackBarMessage = null)
+    }
+
+}
+
+data class MkUiState(
+    val mkEvent: MkEvent = MkEvent(),
+    val isEntryValid: MkErrorState = MkErrorState(),
+    val snackBarMessage: String? = null
+)
+
+data class MkErrorState(
+    val kode: String? = null,
+    val nama: String? = null,
+    val sks: String? = null,
+    val semester: String? = null,
+    val jenis: String? = null,
+    val dosenPengampu: String? = null
+){
+    fun isValid(): Boolean{
+        return kode == null && nama == null && sks == null &&
+                semester == null && jenis == null && dosenPengampu == null
+    }
+}
+
+fun MkEvent.toMkEntity(): MataKuliah = MataKuliah(
+    kode = kode,
+    nama = nama,
+    sks = sks,
+    semester = semester,
+    jenis = jenis,
+    dosenPengampu = dosenPengampu
+)
+
+data class MkEvent(
+    val kode: String = "",
+    val nama: String = "",
+    val sks: String = "",
+    val semester: String = "",
+    val jenis: String = "",
+    val dosenPengampu: String = ""
+)
